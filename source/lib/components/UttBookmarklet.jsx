fa58b80807fe86589cca011ee439aa3f5ada25c1
Wilco Fiers <wilco@pangeadc.nl>
2015-02-24 18:19:40 +0100

    Started on panel title

--- source/lib/components/UttBookmarklet.jsx
+++ source/lib/components/UttBookmarklet.jsx
@@ -1,11 +1,18 @@
 define(['React', './Panel', './TabPanel'],
 function (React, Panel, TabPanel) {
+
 	let Tab = TabPanel.Tab;
+	let iconSrc = require.toUrl(
+		"UTT/components/assets/images/utt-icon.png"
+	);
 
 	let UttBookmarklet = React.createClass({
 		render() {
 			return <Panel>
-				<h1>Hello Bookmarklet</h1>
+				<h1 className="title">
+					<img src={iconSrc} alt="UTT" width="12" height="12" />
+					Hello Bookmarklet
+				</h1>
 				<TabPanel>
 				  <Tab name="Foo">Hello Foo</Tab>
 				  <Tab name="Bar">Hello Bar</Tab>
