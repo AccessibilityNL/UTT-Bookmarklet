585adbb509f74bee9adcf9a04d0556d26183e4a9
Wilco Fiers <wilco@pangeadc.nl>
2015-03-11 17:44:21 +0100

    new build

--- build/bookmarklet.concat.js
+++ build/bookmarklet.concat.js
@@ -137,7 +137,7 @@ define({
 
 var _toConsumableArray = function (arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } };
 
-define(["React", "UTT/components/questioner", "./questiondata", "UTT/utils/highlighter", "UTT/utils/assertion"], function (React, QuestionerElm, questionData, highlighter, assertion) {
+define(["React", "UTT/components/Questioner", "./questiondata", "UTT/utils/highlighter", "UTT/utils/assertion"], function (React, QuestionerElm, questionData, highlighter, assertion) {
 
     function isDecending(parent, child) {
         var node = child.parentNode;
