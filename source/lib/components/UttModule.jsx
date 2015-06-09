d56a95035d1ebe7c2b85f517dcdfc1f77b4807df
Wilco Fiers <wilco@pangeadc.nl>
2015-06-09 16:28:40 +0200

    Updated UttModel component

--- source/lib/components/UttModule.jsx
+++ source/lib/components/UttModule.jsx
@@ -1,13 +1,23 @@
-define(['React', 'UTT/main'],
-function (React, UTT) {
+define(['React', 'UTT/main', './Header'],
+function (React, UTT, Header) {
 
 	let UttModule = React.createClass({
 		render() {
-			return <div className="module">
-				<h2>Title of the booklet</h2>
-				<button onClick={UTT.showHome}>Home</button>
-				<div>{this.props.children}</div>
+			return <div className={this.props.className + " module"}>
+				<Header></Header>
+
+				<div class="content">
+					{this.props.children}
+				</div>
+
+				<div className="footer"><p>
+				  <a href="" onClick={this.home}>Back to overview</a>
+				</p></div>
 			</div>;
+		},
+		home(e) {
+			e.preventDefault();
+			UTT.showHome();
 		}
 	});
 
