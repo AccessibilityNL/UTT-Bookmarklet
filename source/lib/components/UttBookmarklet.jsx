90a13b75ca6a808e658400a05f6bb418ee6c8795
Wilco Fiers <wilco@pangeadc.nl>
2015-06-29 18:24:24 +0200

    setup for animations

--- source/lib/components/UttBookmarklet.jsx
+++ source/lib/components/UttBookmarklet.jsx
@@ -1,10 +1,15 @@
 define(['React', './Panel', 'UTT/utils/browser-polyfill'],
 function (React, Panel) {
+
+	let ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+	console.log('animation not working because CSS is not yet loaded');
+
     let UttBookmarklet = React.createClass({
         render() {
-            return <Panel>
-                {this.props.children}
-            </Panel>;
+            return <ReactCSSTransitionGroup transitionName="example"
+            		transitionAppear={true}>
+            	<Panel>{this.props.children}</Panel>
+        	</ReactCSSTransitionGroup>;
         }
     });
     return UttBookmarklet;
