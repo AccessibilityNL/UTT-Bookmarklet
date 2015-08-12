d910df1ee773e4722ccef0fa9b15d94e8aca3ffe
Wilco Fiers <wilco@pangeadc.nl>
2015-08-12 15:47:45 +0200

    Start work on audio control test

--- source/lib/autoWcag/testCases/SC1-4-2audio-control-audio.js
+++ source/lib/autoWcag/testCases/SC1-4-2audio-control-audio.js
@@ -1,94 +1,123 @@
 define({
-  'id': 'auto-wcag:SC1-4-2-audio-control-audio',
-  'type': 'TestCriterion',
-  'name': 'Audio control',
-  'isPartOf': 'wcag20:visual-audio-contrast-dis-audio',
-  'mode': 'earl:semiAuto',
-  'environment': 'auto-wcag:environment/dom',
-  'subject_type': 'auto-wcag:webpages/single_web_page',
-  'user_expertise': 'auto-wcag:expertise/none',
-  'user_profile': ['auto-wcag:profile/hearing'],
-  'selector': {
-    'type':  'css',
-    'value': 'audio, video'
-  },
-  'steps': [
-    {
-      'start': true,
-      'name': 'step 1',
-      'type': 'automatic' 
-      // check paused property (audio|video.paused).
-      function stepPaused(variables) {
-        var elm = variables.element;
-        if (elm.hasAttribute('paused')) {
-          return {type: 'returnEarl', value: 'earl:passed'};
-        } else {
-          return {type: 'changeStep', value: 'step 2'};
+    'id': 'auto-wcag:SC1-4-2-audio-control-audio',
+    'type': 'TestCriterion',
+    'name': 'Audio control',
+    'isPartOf': 'wcag20:visual-audio-contrast-dis-audio',
+    'mode': 'earl:semiAuto',
+    'environment': 'auto-wcag:environment/dom',
+    'subject_type': 'auto-wcag:webpages/single_web_page',
+    'user_expertise': 'auto-wcag:expertise/none',
+    'user_profile': ['auto-wcag:profile/hearing'],
+    'selector': {
+        'type': 'css',
+        'value': 'audio, video'
+    },
+    'steps': [{
+        'start': true,
+        'name': 'step 1',
+        'type': 'automatic',
+        // check paused property (audio|video.paused).
+        'method': function stepPaused(variables) {
+            var elm = variables.element;
+            if (elm.hasAttribute('paused')) {
+                return {
+                    type: 'returnEarl',
+                    value: 'earl:passed'
+                };
+            } else {
+                return {
+                    type: 'changeStep',
+                    value: 'step 2'
+                };
+            }
         }
-      }
-    }
-    {
-      'name': 'step 2',
-      'type': 'automatic',
-      // Check muted property (audio|video.muted).
-      function stepMuted(variables) {
-        var elm = variables.element;
-        if (elm.hasAttribute('muted')) {
-          return {type: 'returnEarl', value: 'earl:passed'};
-        } else {
-          return {type: 'changeStep', value: 'step 3'};
+    }, {
+        'name': 'step 2',
+        'type': 'automatic',
+        // Check muted property (audio|video.muted).
+        'method': function stepMuted(variables) {
+            var elm = variables.element;
+            if (elm.hasAttribute('muted')) {
+                return {
+                    type: 'returnEarl',
+                    value: 'earl:passed'
+                };
+            } else {
+                return {
+                    type: 'changeStep',
+                    value: 'step 3'
+                };
+            }
         }
-      }
-    }
-    {
-      'name': 'step 3',
-      'type': 'automatic',
-      // Check duration property is no longer than 3 seconds (audio|video.duration).
-      function stepDuration(variables) {
-        var elm = variables.element;
-        if (elm.duration < 3) {      
-          return {type: 'returnEarl', value: 'earl:passed'};
-        } else {
-          return {type: 'changeStep', value: 'step 4'};
+    }, {
+        'name': 'step 3',
+        'type': 'automatic',
+        // Check duration property is no longer than 3 seconds (audio|video.duration).
+        'method': function stepDuration(variables) {
+            var elm = variables.element;
+            if (elm.duration < 3) {
+                return {
+                    type: 'returnEarl',
+                    value: 'earl:passed'
+                };
+            } else {
+                return {
+                    type: 'changeStep',
+                    value: 'step 4'
+                };
+            }
         }
-      }
-    }
-    {
-      'name': 'step 4',
-      'type': 'automatic',
-      // Check loop property (audio|video.loop).
-      function stepLoop(variables) {
-        var elm = variables.element;
-        if (elm.hasAttribute('loop')) {
-          return {type: 'returnEarl', value: 'earl:passed'};
-        } else {
-          return {type: 'changeStep', value: 'step 5'};
+    }, {
+        'name': 'step 4',
+        'type': 'automatic',
+        // Check loop property (audio|video.loop).
+        'method': function stepLoop(variables) {
+            var elm = variables.element;
+            if (elm.hasAttribute('loop')) {
+                return {
+                    type: 'returnEarl',
+                    value: 'earl:passed'
+                };
+            } else {
+                return {
+                    type: 'changeStep',
+                    value: 'step 5'
+                };
+            }
         }
-      }
-    }
-    {
-      'name': 'step 5',
-      'type': 'userQuestion',
-      'question': {
-        'questiontext': 'Is there audio playing on the web page?',
-        'helptext': 'There should not be audio playing automatically on the web page loads.',
-        'answers': {
-          'yes': {type: 'changeStep', value: 'step 6'}
-          'no': {type: 'returnEarl', value: 'earl:passed'}
+    }, {
+        'name': 'step 5',
+        'type': 'userQuestion',
+        'question': {
+            'questiontext': 'Is there audio playing on the web page?',
+            'helptext': 'There should not be audio playing automatically on the web page loads.',
+            'answers': {
+                'yes': {
+                    type: 'changeStep',
+                    value: 'step 6'
+                },
+                'no': {
+                    type: 'returnEarl',
+                    value: 'earl:passed'
+                }
+            }
         }
-      }
-    }
-    {
-      'name': 'step 6',
-      'type': 'userQuestion',
-      'question': {
-        'questiontext': 'Does the web page provide a mechanism to control the sound as one of the first five links or buttons?',
-        'helptext': 'A mechanism to pause or stop the video or audio, or control the volume or mute the audio must be available on the web page. The mechanism must be located as one of the first five links or buttons of the web page. This way people (with screen readers) can turn off the sound before reading the web page. To inspect this, use the tab key to navigate through the web page.',
-        'answers': {
-          'yes': {type: 'returnEarl', value: 'earl:passed'}
-          'no': {type: 'returnEarl', value: 'earl:failed'}
+    }, {
+        'name': 'step 6',
+        'type': 'userQuestion',
+        'question': {
+            'questiontext': 'Does the web page provide a mechanism to control the sound as one of the first five links or buttons?',
+            'helptext': 'A mechanism to pause or stop the video or audio, or control the volume or mute the audio must be available on the web page. The mechanism must be located as one of the first five links or buttons of the web page. This way people (with screen readers) can turn off the sound before reading the web page. To inspect this, use the tab key to navigate through the web page.',
+            'answers': {
+                'yes': {
+                    type: 'returnEarl',
+                    value: 'earl:passed'
+                },
+                'no': {
+                    type: 'returnEarl',
+                    value: 'earl:failed'
+                }
+            }
         }
-      }
-    }
-  ]
+    }]
 });
--- source/lib/config.js
+++ source/lib/config.js
@@ -19,18 +19,24 @@ define([
         'keyboard'
     ];
 
+    const assessorTests = {
+        images: ["autoWcag/testCases/SC1-1-1aria-describedby"],
+        media: ["autoWcag/testCases/SC1-4-2audio-control-audio"],
+        language: ["autoWcag/testCases/SC1-1-1aria-describedby"],
+        navigation: ["autoWcag/testCases/SC1-1-1aria-describedby"],
+        keyboard: ["autoWcag/testCases/SC1-1-1aria-describedby"]
+    };
+
     let config = {
         apiUrl:  'http://utt-dev.huell.appnormal.com/v1',
         modules: assessorNames.map(assessorName => ({
                 "controller": 'UTT/modules/assessor/assessor',
                 "config": {
+                    "category":       assessorName,
                     'icon':           `icon-${assessorName}.svg`,
                     'iconIncomplete': `icon-${assessorName}-report-0.svg`,
                     'iconComplete':   `icon-${assessorName}-report-1.svg`,
-                    "questions":      [
-                        "autoWcag/testCases/SC1-1-1aria-describedby"
-                    ],
-                    "category":  assessorName
+                    "questions":      assessorTests[assessorName]
                 },
                 locale: Object.assign(
                         require('UTT/locale/assessor/' + assessorName),
